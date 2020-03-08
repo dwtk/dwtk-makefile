3e9a99bda7c535cf09396b0212c6597b3185487e
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2020-03-08 23:34:08 +0100

    initial code

---
+++ README.md
@@ -0,0 +1,15 @@
+# dwtk-makefile
+
+A generic Makefile for AVR projects using dwtk.
+
+
+## How to use
+
+Just copy `Makefile` and `dwtk-config.mk` files to your project and adapt `dwtk-config.mk` to fit your project. The settings are mostly self-explanatory.
+
+The example `dwtk-config-local.example.mk` local configuration file is also useful, but not required.
+
+
+## Usage examples
+
+There's a very simple example in the `examples/` folder. The [`dwtk-ice`](https://github.com/dwtk/dwtk-ice) firmware is also a good example.
---
+++ dwtk-config-local.example.mk
@@ -0,0 +1,23 @@
+#
+# dwtk-makefile: A generic Makefile for AVR projects using dwtk.
+# Copyright (C) 2020 Rafael G. Martins <rafael@rafaelmartins.eng.br>
+#
+# This program can be distributed under the terms of the BSD 3-Clause License.
+# See https://opensource.org/licenses/BSD-3-Clause.
+#
+
+# This is an example local configuration. The example can be commited to
+# the version control system, but the actual dwtk-config-local.mk file
+# shouldn't. Users should copy this file to dwtk-config-local.mk and
+# edit that instead. This file is evaluated after dwtk-config.mk and 
+# can override anything set there.
+
+# Enable dwtk debug flag (verbose logging of operations)
+#DWTK_DEBUG = 1
+
+# When using USB-To-TTL devices, configure the serial port device. When
+# setting this, any dwtk-ice device plugged is ignored.
+#DWTK_SERIAL = /dev/ttyUSB0
+
+# Build firmware for release (with optimizations, without debug helpers)
+#AVR_RELEASE = 1
